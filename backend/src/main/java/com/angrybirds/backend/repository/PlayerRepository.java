028e41b32fb7ab4185474da15fb67e0dafeee6f2
na-rai <nazraisa2510@gmail.com>
2026-06-06 23:20:45 +0600

    Add files via upload

---
+++ backend/src/main/java/com/angrybirds/backend/repository/PlayerRepository.java
@@ -0,0 +1,7 @@
+package com.birdgame.backend.repository;
+
+import com.birdgame.backend.model.Player;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlayerRepository extends JpaRepository<Player, Long> {
+}
---
+++ backend/src/main/java/com/angrybirds/backend/repository/ScoreRepository.java
@@ -0,0 +1,9 @@
+package com.birdgame.backend.repository;
+
+import com.birdgame.backend.model.Score;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface ScoreRepository extends JpaRepository<Score, Long> {
+    List<Score> findTop10ByOrderByScoreDesc();
+}
