b2534c3b873fdf79eae9e43d3bea963bb7d2ccc3
na-rai <nazraisa2510@gmail.com>
2026-09-20 21:57:44 +0600

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,21 +1,20 @@
 import Phaser from 'phaser'
-
-import { GameScene2 } from './scenes/Gamescene2.js'
-import { FlashcardScene2 } from './scenes/FlashcardScene2.js'
-import { GameScene3 } from './scenes/GameScene3.js'
-import { ReportScene } from './scenes/ReportScene.js'
-import { FlashCardScene } from './scenes/FlashCardScene.js'
-import { ShowcaseScene } from './scenes/ShowcaseScene.js'
 import { MenuScene }  from './scenes/MenuScene.js'
 import { StoryScene } from './scenes/StoryScene.js'
 import { GameScene }  from './scenes/GameScene.js'
+import { GameScene2 } from './scenes/GameScene2.js'
+import { GameScene3 } from './scenes/GameScene3.js'
 import { UIScene }    from './scenes/UIScene.js'
-import { LastScene }  from './scenes/LastScene.js'
+import { FlashcardScene }    from './scenes/FlashcardScene.js'
+import { FlashcardScene2 }    from './scenes/FlashcardScene2.js'
+import { Showcasescene }    from './scenes/Showcasescene.js'
+import { LastScene }    from './scenes/LastScene.js'
+import { ReportScene }    from './scenes/ReportScene.js'
 
 const config = {
   type: Phaser.AUTO,
   parent: 'app',
-  backgroundColor: '#000000',
+  backgroundColor: '#2d5a1b',
   scale: {
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH,
@@ -26,19 +25,7 @@ const config = {
     default: 'arcade',
     arcade: { gravity: { y: 0 }, debug: false }
   },
-  scene: [
-    MenuScene,
-    StoryScene,
-    GameScene,
-    ReportScene,
-    FlashCardScene,
-    ShowcaseScene,
-    GameScene2,
-    FlashcardScene2,
-    GameScene3,
-    UIScene,
-    LastScene
-  ]
+  scene: [MenuScene, StoryScene, GameScene, GameScene2, GameScene3, UIScene, FlashcardScene,FlashcardScene2, Showcasescene,LastScene,ReportScene]
 }
 
 new Phaser.Game(config)
